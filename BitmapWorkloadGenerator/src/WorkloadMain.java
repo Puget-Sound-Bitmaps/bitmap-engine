8b79f607ee6444ea1787989e8105ebe496e2a208
Jahrme Risner <github@jahrme.com>
2018-07-16 15:12:48 -0700

    Make WorkloadMain easier to understand

--- BitmapWorkloadGenerator/src/WorkloadMain.java
+++ BitmapWorkloadGenerator/src/WorkloadMain.java
@@ -1,35 +1,83 @@
-public class WorkloadMain
-{
-    public static void main(String[] args)
-    {
-        //*************SET GENERAL VARIABLES
-        int num_attributes = 10; // number of attributes
-        int cardinality = 10; // cardinality (number of bins per attribute)
-        //*************END
-        //*************SET VARIABLES HERE FOR BITMAP DATA GENERATION
-        int num_rows = 30; // number of rows
-        int data_skew = 2; // skew for which bins are chosen to be favored with a 1
-        // where 0 = uniform distribution and infinity = first rank always gets picked
-        String bitmap_out = "bitmap_out.txt"; // name of the file containing the bitmap
-        String bitmap_out_gc = "bitmap_out_gc.txt"; // name of the file containing the grey code-ordered bitmap
-        //**************END
-        //generate the bitmap file
+/**
+ * Generates bitmaps and queries for bitmap experiments.
+ */
+public class WorkloadMain {
+    /*********************
+     * Output File Names *
+     *********************/
+
+    /** Name of the ouput file containing the bitmaps. */
+    public static final String bitmap_out = "bitmap_out.txt";
+
+    /** Name of the ouput file containing the grey code-ordered bitmaps. */
+    public static final String bitmap_out_gc = "bitmap_out_gc.txt";
+
+    /** Name of the output file containing the queries. */
+    public static final String query_out = "query_out.txt";
+
+    /*****************
+     * Bitmap Fields *
+     *****************/
+
+    /** Number of attributes. */
+    public static final int num_attributes = 10000;
+
+    /** Cardinality (number of bins per attribute). */
+    public static final int cardinality = 10;
+
+    /** Number of rows. */
+    public static final int num_rows = 1048576;
+
+    /**
+     * Skew for which bins are chosen to be favored with a 1.
+     * A value of 0 implies uniform distribution.
+     * A value of "infinity" implies the first rank always being picked.
+     */
+    public static final int data_skew = 2;
+
+    /****************
+     * Query Fields *
+     ****************/
+
+    /**
+     * The query mode; must be one of:
+     *     QueryGenerator.POINT_ONLY
+     *     QueryGenerator.RANGE_ONLY
+     *     QueryGenerator.MIXED_MODE
+     */
+    public static final int mode = QueryGenerator.MIXED_MODE;
+
+    /** Number of queries to generate. */
+    public static final int num_queries = 1000;
+
+    /**
+     * The ratio of queries to be point queries.
+     * Thus, a value of 0.7 implies queries containing 70% point queries.
+     * Only applicable in MIXED_MODE.
+     */
+    public static final double query_ratio = 0.7;
+
+    /**
+     * Skew for which attributes and bins are to be queried.
+     * For more information, see explanation of {@code data_skew} above.
+     */
+    public static final int qry_skew_att = 2;
+    public static final int qry_skew_bin = 2;
+
+    public static void main(String[] args) {
+        // Bitmaps
+        System.out.println("Generating Bitmaps");
         DataGenerator data = new DataGenerator(num_attributes, cardinality, data_skew, num_rows);
         data.writeFile(bitmap_out);
-        //sort the file using Grey code ordering
+
+        // Grey Code-Ordered Bitmaps
+        System.out.println("Generating Grey Code-Ordered Bitmaps");
         GreyCodeSorter sorter = new GreyCodeSorter(bitmap_out);
         sorter.writeFile(bitmap_out_gc);
-        //*************SET VARIABLES HERE FOR QUERY SET GENERATION
-        //int mode = QueryGenerator.POINT_ONLY; // must be one of {POINT_ONLY, RANGE_ONLY, MIXED_MODE}
-        //int mode = QueryGenerator.RANGE_ONLY; // must be one of {POINT_ONLY, RANGE_ONLY, MIXED_MODE}
-        int mode = QueryGenerator.MIXED_MODE; // must be one of {POINT_ONLY, RANGE_ONLY, MIXED_MODE}
-        int num_queries = 1000; // number of queries to generate
-        String query_out = "query_out.txt"; // name of the file containing the queries
-        int qry_skew_att = 2;
-        int qry_skew_bin = 2;
-        //**************END
-        //generate the query file
+
+        // Queries
+        System.out.println("Generating Queries");
         QueryGenerator query = new QueryGenerator(num_attributes, cardinality, num_queries, qry_skew_att, qry_skew_bin);
-        query.writeFile(mode, 0.7, query_out); //query set containing 70% point queries
+        query.writeFile(mode, query_ratio, query_out);
     }
 }
